84ebc33f881ff65e5263b3929a9d649dfc819bf8
Sabilul Hikam <billhikam2003@gmail.com>
2026-08-16 11:02:37 +0700

    wip: integrate fnb transaction modifier with backend

--- api/src/controllers/transaction.controller.ts
+++ api/src/controllers/transaction.controller.ts
@@ -345,11 +345,11 @@ const endpoint = {
           },
           transactionItemFnbs: {
             select: {
+              id: true,
               quantity: true,
               sub_total: true,
               fnb_item: {
                 select: {
-                  id: true,
                   title: true,
                   description: true,
                   price: true,
@@ -443,6 +443,71 @@ const endpoint = {
       res.status(Number(midtransError.httpStatusCode) ?? 500).json({
         message: "failed-to-generate-qris",
         detail: midtransError.ApiResponse ?? midtransError.message,
+      });
+    }
+  },
+
+  addFnbItem: async (req: Request, res: Response) => {
+    const transactionId = Number(req.body.transaction_id);
+    const fnbId = req.body.fnb_id;
+
+    try {
+      const selectedFnb = await prisma.fnb_Item.findUniqueOrThrow({
+        where: { id: fnbId },
+      });
+      const existingFnbTransaction =
+        await prisma.transaction_Item_Fnb.findFirst({
+          where: {
+            transaction_id: transactionId,
+            fnb_item_id: fnbId,
+          },
+        });
+
+      let newFnbTransaction: any;
+      if (!existingFnbTransaction) {
+        newFnbTransaction = await prisma.transaction_Item_Fnb.create({
+          data: {
+            transaction_id: transactionId,
+            fnb_item_id: fnbId,
+            quantity: 1,
+            sub_total: selectedFnb.price,
+          },
+        });
+      } else {
+        const oldQuantity = existingFnbTransaction.quantity;
+        newFnbTransaction = await prisma.transaction_Item_Fnb.update({
+          where: {
+            id: existingFnbTransaction.id,
+          },
+          data: {
+            quantity: oldQuantity + 1,
+            sub_total: selectedFnb.price * (oldQuantity + 1),
+          },
+        });
+      }
+      res.json({
+        newFnbTransaction,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "error-update-data",
+      });
+    }
+  },
+
+  removeFnBTransaction: async (req: Request, res: Response) => {
+    const fnbTransactionId = Number(req.params.id);
+
+    try {
+      await prisma.transaction_Item_Fnb.delete({
+        where: { id: fnbTransactionId },
+      });
+      res.json({
+        message: "fnb-transaction-deleted",
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "error-delete-data",
       });
     }
   },
--- api/src/routes/transaction.route.ts
+++ api/src/routes/transaction.route.ts
@@ -10,6 +10,8 @@ route.get(
 );
 route.post("/proceed-payment/:id", transactionController.proceedPayment);
 route.post("/generate-qris/:id", transactionController.generateQris);
+route.post("/add-fnb", transactionController.addFnbItem);
+route.get("/remove-fnb/:id", transactionController.removeFnBTransaction);
 route.get("/:id", transactionController.getDetail);
 route.post("/", transactionController.create);
 
